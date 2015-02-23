79d2e33177723cc819441bd47046e65427b51e03
krismaz <krislinux@gmail.com>
2015-02-23 20:34:57 +0100

    More tests

--- dailyprogrammer/testset.py
+++ dailyprogrammer/testset.py
@@ -6,5 +6,9 @@ tests = [
 	(2, 'medium', ['Trip', 'Void', 'Drown', 'Grue', 'Win']), #Text-based RPG
 	(2, 'hard', []), #Stopwatch, no tests due to timing
 	(3, 'easy', ['Simple', 'Case', 'Loop', 'Reverse']), #Caesar cipher
-	(3, 'medium', ['Encrypt', 'Decrypt']) #Substituion Cipher
+	(3, 'medium', ['Encrypt', 'Decrypt']), #Substituion Cipher
+	(3, 'hard', ['Words']), #Anagram lookup
+	(4, 'easy', []), #Random password generator, no way to match
+	(4, 'medium', ['AddSubtract', 'Multiply', 'Divide', 'Pow', 'Parens', 'Big']), #Calculator
+	(4, 'hard', ['Base', 'Three', 'Four']), #Equality matching
 	]
