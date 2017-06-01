68dede44e22ba407630e493fb3fc22e52adc57a0
krismaz <krislinux@gmail.com>
2017-06-01 13:02:06 +0200

    198

--- dailyprogrammer/198/198.py
+++ dailyprogrammer/198/198.py
@@ -2,15 +2,15 @@
 
 from itertools import combinations
 
-with open('dictionary', errors='ignore') as f:
+with open('da-dict.txt', errors='ignore') as f:
     content = f.readlines()
 
 lookup = dict((''.join(sorted(i[:-1].lower())),i[:-1].lower()) for i in content)
 
-inputs = 'zombies'
+inputs = 'odeiyqhndyeusbeleaontrmim'
 
 keys = lookup.keys()
 
-for comb in [''.join(sorted(c)) for i in range(3, len(inputs)+1) for c in combinations(inputs, i)]:
+for comb in [''.join(sorted(c)) for i in range(3, 15) for c in combinations(inputs, i)]:
 	if comb in keys:
 		print(lookup[comb])
